eb02cf3c56dceafdc0e72ba0323a1fdd4ca1a224
Matheus Souza <joaomtfslp123@gmail.com>
2025-12-17 10:09:35 -0400

    atualização

--- App/Controllers/ContasUsuariosController.php
+++ App/Controllers/ContasUsuariosController.php
@@ -327,7 +327,7 @@ class ContasUsuariosController extends ControllerBase
     }
   }
 
-  public function update($data)
+  public function updateOnly($data)
   {
     try {
       $currentData = $this->model->current();
@@ -389,12 +389,22 @@ class ContasUsuariosController extends ControllerBase
 
         $result = $this->model->update($data);
 
-        http_response_code(200);
-        echo json_encode($result);
+        return $result;
       } else {
-        http_response_code(404);
-        echo json_encode(["message" => "User not found"]);
+        throw new \Exception("Conta de usuário não encontrada");
       }
+    } catch (\Exception $e) {
+      throw new \Exception($e->getMessage());
+    }
+  }
+
+  public function update($data)
+  {
+    try {
+      $result = $this->updateOnly($data);
+
+      http_response_code(200);
+      echo json_encode($result);
     } catch (\Exception $e) {
       http_response_code(500);
       echo json_encode(["message" => $e->getMessage()]);
